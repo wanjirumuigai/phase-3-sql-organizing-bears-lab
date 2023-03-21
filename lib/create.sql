3a828ad1a5719d362061eb9114b82a96ed95efcb
Pauline Muigai <wanjirumuigai@gmail.com>
2023-03-21 21:59:08 +0300

    completed lab

---
+++ lib/create.sql
@@ -0,0 +1,1 @@
+CREATE TABLE bears (id integer PRIMARY KEY, name text, age integer, sex text, color text, temperament text, alive boolean);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+insert into bears (name, age, sex, color, temperament,alive) values ("Mr. Chocolate", 5, "F", "black","wild",1),
+("Rowdy", 7, "F", "black","wild",0),
+("Tabitha", 7, "F", "brown","friendly",0),
+("Sergeant Brown", 1, "M", "brown","friendly
+",0),
+("Melissa", 1, "F", "white","friendly",1),
+("Grinch", 1, "M", "green","sour",1),
+("Wendy", 10, "F", "green","happy",1),
+(null, 15, "F", "black","old_soul",1)
+ ;
